6970772028e18139e70e03396d039a14c7b05711
Jun Anthony <Unknown@DESKTOP-LAR4J4U>
2024-06-30 11:43:04 +1200

    Add EmployeeDepartmentController

---
+++ LINQ-Practice4/Controllers/EmployeeDepartmentController.cs
@@ -0,0 +1,19 @@
+﻿using LINQ_Practice4.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LINQ_Practice4.Controllers
+{
+    public class EmployeeDepartmentController : Controller
+    {
+        private readonly IEmployeeDepartmentRepository _employeeDepartmentRepository;
+        public EmployeeDepartmentController(IEmployeeDepartmentRepository employeeDepartmentRepository)
+        {
+            _employeeDepartmentRepository = employeeDepartmentRepository;
+        }
+        public IActionResult Index()
+        {
+            var employeeDepartment = _employeeDepartmentRepository.GetEmployeeDepartment();
+            return View(employeeDepartment);
+        }
+    }
+}
